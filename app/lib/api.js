2bba1da28d8b16dc599e11f7ba9901cb4d4b93df
sheikhhaad <sheikhhaad6@gmail.com>
2026-03-30 17:59:19 +0500

    .

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
-  // baseURL: "https://stu-portal-backend.vercel.app/api",
+  // baseURL: "http://localhost:8000/api",
+  baseURL: "https://stu-portal-backend.vercel.app/api",
   withCredentials: true,
 });
 
