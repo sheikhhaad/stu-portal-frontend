ad707df9f41753182e07dd5605b92f5b3c61d315
sheikhhaad <sheikhhaad6@gmail.com>
2026-03-31 18:50:11 +0500

    ..

--- app/lib/socket.js
+++ app/lib/socket.js
@@ -1,8 +1,14 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://stu-portal-backend.vercel.app");
+const socket = io("https://stu-portal-backend.vercel.app", {
+  autoConnect: false,
+});
+
+if (typeof window !== "undefined") {
+  socket.connect();
+}
 
 socket.on("connect", () => console.log("Socket connected:", socket.id));
-socket.on("connect_error", (err) => console.log("Socket error:", err));
+socket.on("connect_error", (err) => console.log("Socket error:", err.message));
 
 export default socket;
